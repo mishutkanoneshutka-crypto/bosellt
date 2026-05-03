6be4806bdd053ae86bf4834a0bfd0e605bae5c1a
mishutkanoneshutka-crypto <mishutkanoneshutka@gmail.com>
2026-05-03 05:52:42 -0700

    Update init_db.py

--- database/init_db.py
+++ database/init_db.py
@@ -1,6 +1,7 @@
-from database.base import Base, engine
+from database.base import Base
+import database.base as db
 
 
 async def init_db() -> None:
-    async with engine.begin() as conn:
+    async with db.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
